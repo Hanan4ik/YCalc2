ad35c52905f7a51961a768498ad73649539f75d9
Hanan4ik <makc09022007@gmail.com>
2025-06-23 13:58:17 +0300

    Add multiplication func

--- Calculators/Calc.py
+++ Calculators/Calc.py
@@ -1,10 +1,10 @@
 from Calculators.ICalc import ICalc
-from fnmatch import fnmatch
+
+
 class Calc(ICalc):
 
     def __init__(self):
         super().__init__()
-
 
     def input(self, expression):
         super().input(expression)
@@ -24,18 +24,21 @@ class Calc(ICalc):
         i = 1
         len_exp = len(self.expression)
         while i < len_exp:
-            if self.expression[i] == "(" and self.expression[i-1].isdigit():
+            if self.expression[i] == "(" and self.expression[i - 1].isdigit():
                 self.expression = self.expression[:i] + "*" + self.expression[i:]
-            elif i+1 != len_exp and self.expression[i] == ")"  and self.expression[i+1].isdigit():
-                self.expression = self.expression[:i+1] + "*" + self.expression[i+1:]
+            elif i + 1 != len_exp and self.expression[i] == ")" and self.expression[i + 1].isdigit():
+                self.expression = self.expression[:i + 1] + "*" + self.expression[i + 1:]
             else:
                 i += 1
         del i, len_exp
 
+        # Replace plus/minus sign into add function
+
         if "+" not in self.expression and "-" not in self.expression:
             return
 
-        # Replace plus/minus sign into add function
+        def to_func():
+            pass
 
 
 
@@ -51,20 +54,9 @@ class Calc(ICalc):
         except Exception:
             return "Error while calculating"
 
-
-
-
-        if type(self.result) == float and self.result.as_integer_ratio()[1] == 1:
+        if isinstance(self.result, float) and self.result.as_integer_ratio()[1] == 1:
             self.result = self.result.as_integer_ratio()[0]
-
-
-
-
-
-
-
 
 
 a = Calc()
 print(a.input("(132+64) - 33*64(128/5) + 3**2 + 3"))
-
--- Functions/base_funcs.py
+++ Functions/base_funcs.py
@@ -2,7 +2,7 @@ from math import *
 
 # Sum of float numbers to avoid strange results
 # (Add description)
-def add(x: float, y: float):
+def add(x, y):
     s_x = str(x)
     x_dot = s_x.find(".")
     s_y = str(y)
@@ -16,8 +16,28 @@ def add(x: float, y: float):
         return (x + y)//mul
     return (x + y) / mul
 
+def mul(x: float, y: float):
+    s_x = str(x)
+    x_dot = s_x.find(".")
+    if x_dot == -1:
+        x_dot = 0
+    s_y = str(y)
+    y_dot = s_y.find(".")
+    if y_dot == -1:
+        y_dot = 0
+    x_remain = len(s_x)-x_dot-1
+    y_remain = len(s_y)-y_dot-1
+
+    mul_ = 10**(x_remain+y_remain)
+    x = int(s_x.replace(".", ""))
+    y = int(s_y.replace(".", ""))
+    if x_dot == y_dot == 0:
+        return (x * y)//mul_
+    return (x * y) / mul_
+
 if __name__ == "__main__":
-    print(add(123.123, -312.1234))
+    print(add(0.1, 6))
+    print(mul(1.1213, 6))
 
 
 
